deafe503424bd6a701cf6734caca3dd81c68c202
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2024-08-15 21:44:49 +0545

    route audit

--- routes/auditRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const auditController = require("../controller/auditController");
-const { authGuard } = require("../middleware/authguard");
-
-router.get('/logs', authGuard, auditController.getAuditLogs);
-
-module.exports = router
